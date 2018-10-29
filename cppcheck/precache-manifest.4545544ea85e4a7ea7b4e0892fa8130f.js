9003282f7a7864e8082e6e8bed2cb3f67cfd55f8
Vladimir Rutsky <rutsky@google.com>
2018-10-29 06:40:24 +0100

    Add static version of classroom progress

---
+++ cppcheck/precache-manifest.4545544ea85e4a7ea7b4e0892fa8130f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b80a59247e7c591b74a",
+    "url": "/cppcheck/static/js/runtime~main.4b80a592.js"
+  },
+  {
+    "revision": "9411c083333785d0e551",
+    "url": "/cppcheck/static/js/main.9411c083.chunk.js"
+  },
+  {
+    "revision": "81ac6c9a7d9d5e1501fa",
+    "url": "/cppcheck/static/js/1.81ac6c9a.chunk.js"
+  },
+  {
+    "revision": "9411c083333785d0e551",
+    "url": "/cppcheck/static/css/main.279e92a6.chunk.css"
+  },
+  {
+    "revision": "056537faddaa4a54fb06208eea2a0251",
+    "url": "/cppcheck/index.html"
+  }
+];
